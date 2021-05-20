607b8131c193ec321d12f6ef04fc80dfcfd17458
PranavMiglani45 <Pranav0187.cse19@chitkara.edu.in>
2021-05-20 18:07:19 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {gameSubject,initGame,resetGame} from './Game';
+import React,{useEffect,useState} from 'react';
+import Board from './Board';
+
 
 function App() {
+  const  [board,setBoard] =useState([ ])
+  const [isGameOver, setIsGameOver] = useState()
+  const [result, setResult] = useState()
+  const [turn, setTurn] = useState()
+  useEffect(() => {
+    initGame()
+    const subscribe = gameSubject.subscribe((game) => {setBoard(game.board)
+    setBoard(game.board)
+    setIsGameOver(game.isGameOver)
+    setResult(game.result)
+    setTurn(game.turn)
+    })
+    return () =>subscribe.unsubscribe();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" d-flex justify-content-center" id="container" >
+      {isGameOver && (
+        <h2 className="vertical-text">
+          GAME OVER 
+          <button onClick={resetGame}>
+            <span className="vertical-text"> NEW GAME</span>
+          </button>
+          </h2>
+      )}
+      <div className="w-600 h-600" id="board-container">
+      <Board board={board} turn={turn}/>
+      </div>
+      {result && <p className="vertical-text">{result}</p>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {DndProvider} from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend'
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <DndProvider backend={HTML5Backend}>
     <App />
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
